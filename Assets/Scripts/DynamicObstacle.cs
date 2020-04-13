59d568163e487d7b1a9f5eebadcdef495efdaf9c
eiden.m <eiden.m@husky.neu.edu>
2020-04-13 14:07:31 -0400

    add dynamic obstacle

---
+++ Assets/Scripts/DynamicObstacle.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicObstacle : MonoBehaviour
+{
+    public float speed = 3;
+    public float distance = 5;
+
+    private Vector3 startPos;
+    
+    void Start()
+    {
+        startPos = transform.position; 
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = startPos.x + (Mathf.Sin(Time.time * speed) * distance);
+
+        transform.position = newPos; 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            other.transform.parent = transform; 
+        }
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            other.transform.parent = transform; 
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            other.transform.parent = null; 
+        }
+    }
+
+}
